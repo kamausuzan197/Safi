49dcaad38c90fd2085a189242cb9cd9592649bc4
kamausuzan197 <kamaususan197@gmail.com>
2022-09-06 07:45:18 +0300

    landing page styling

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+import Navbar from "./components/Navbar";
+import Landing from "./sections/Landing";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
