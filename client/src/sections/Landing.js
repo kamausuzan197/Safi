3f4f11ed4f8fcef891b7de2188bf7f7ab395bd3a
kamausuzan197 <kamaususan197@gmail.com>
2022-09-06 07:44:15 +0300

    landing page section

---
+++ client/src/sections/Landing.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Button from "../components/Button";
+import "./Landing.css";
+
+function Landing() {
+  return (
+    <div className="landing">
+      <div className="ld_left">
+        <h1>
+          Having trable with
+          <br /> laundry place?
+        </h1>
+        <p>
+          Welcome to Safi, This is a website that allows you to place an
+          <br />
+          order and get your clothers cleaned, and delivered to your home.
+        </p>
+        <div className="ld_button">
+          <Button
+            name="Update Order"
+            bgColor="blue"
+            color="white"
+            padding="8px"
+            radius="10px"
+            fontSize="26px"
+          />
+          <Button
+            name="Delete Order"
+            bgColor="white"
+            color="black"
+            padding="8px"
+            radius="10px"
+            fontSize="26px"
+          />
+        </div>
+      </div>
+      <div className="ld_right">
+        <img
+          className="landingimg"
+          src="https://previews.123rf.com/images/dit26978/dit269781803/dit26978180300029/98385071-3d-rendering-washing-machine-in-vintage-laundry-room.jpg"
+          alt="laundry"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
