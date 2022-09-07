0bdd065277e18539e4f7f16f62effd3e4a250308
kamausuzan197 <kamaususan197@gmail.com>
2022-09-07 16:05:55 +0300

    review  migration

--- api/db/migrate/20220907050447_create_reviews.rb
+++ api/db/migrate/20220907050447_create_reviews.rb
@@ -1,8 +1,10 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
-  create_table :review do |t|
+  create_table :reviews do |t|
     t.string :comment
     t.integer :ordernumber
+    t.integer :laundry_id
+    t.integer :user_id
   end
   end
 end
