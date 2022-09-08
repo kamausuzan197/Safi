8cac78e46f04df87e9d9cf1b942ef9de432d3997
kamausuzan197 <kamaususan197@gmail.com>
2022-09-08 21:52:08 +0300

    js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import "./App.css"
 import Navbar from "./components/Navbar";
 import Landing from "./sections/Landing";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import OderForm from "./pages/OderForm";
+import UpdateReview from "./pages/UpdateReview";
+import UserForm from "./pages/UserForm";
+import Success from "./pages/Success";
+import Features from "./sections/Features";
 
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Landing />
-    </div>
+     //create a router
+      <div className="app">
+        <BrowserRouter>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/" element={<Features />} />
+                <Route path="/order" element={<OderForm />} />
+                <Route path="/review" element={<UpdateReview />} />
+                <Route path="/user" element={<UserForm />} />
+                <Route path="/success" element={<Success />} />
+            </Routes>
+        </BrowserRouter>
+      </div>
   );
 }
 
