c8f57f14b69504b4bb99f572e6fed31897c38ba4
kamausuzan197 <kamaususan197@gmail.com>
2022-09-08 21:58:54 +0300

    backend

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -2,8 +2,52 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/review" do
+    review = Review.all.order(:variety)
+    review.to_json
   end
+
+  post '/reviews' do
+    review = Review.create(
+    ordernumber: params[:ordernumber],
+    comment: params[:comment],
+    laundry_id: params[:laundry_id],
+    user_id: params[:user_id]
+  )
+  review.to_json 
+  end
+
+  post '/laundry' do
+    laundry = Laundry.create(
+    name: params[:name],
+    variety: params[:variety],
+    price: params[:price]
+  )
+  laundry.to_json 
+  end
+
+  post '/user' do
+    user = User.create(
+    name: params[:name]
+  )
+  user.to_json 
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      ordernumber: params[:ordernumber],
+      comment: params[:comment]
+    )
+    review.to_json
+  end
+
+
 
 end
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,5 +1,7 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+User.create(name:)
+
 
 puts "✅ Done seeding!"
