dbf2feeb34dee2ef8a15c563069b9aa0868f05bc
kamausuzan197 <kamaususan197@gmail.com>
2022-09-09 09:38:51 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import UpdateReview from "./pages/UpdateReview";
 import UserForm from "./pages/UserForm";
 import Success from "./pages/Success";
 import Features from "./sections/Features";
+import Footer from "./components/Footer";
+import DeleteForm from "./pages/DeleteForm";
 
 
 function App() {
@@ -20,9 +22,11 @@ function App() {
                 <Route path="/" element={<Features />} />
                 <Route path="/order" element={<OderForm />} />
                 <Route path="/review" element={<UpdateReview />} />
+                <Route path='/delete' element={<DeleteForm />} />
                 <Route path="/user" element={<UserForm />} />
                 <Route path="/success" element={<Success />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
       </div>
   );
